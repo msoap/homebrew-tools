465b0babf0411bbca912ce7439e808e2dda35d13
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-10-04 23:20:30 +0300

    New version of shell2telegram

--- shell2telegram.rb
+++ shell2telegram.rb
@@ -1,13 +1,13 @@
 class Shell2telegram < Formula
   homepage "https://github.com/msoap/shell2telegram"
-  version '1.0'
+  version '1.1'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/shell2telegram/releases/download/1.0/shell2telegram-1.0.amd64.darwin.zip"
-    sha256 "4b7c7270e8c29f09deccf2966e3e65b948c341dd8c52767e3908776728a08e8e"
+    url "https://github.com/msoap/shell2telegram/releases/download/1.1/shell2telegram-1.1.amd64.darwin.zip"
+    sha256 "15cb1a89b3b24183e9bdcbc11ca96b624826ccbac4d35242e0ffc7a4b6f0d134"
   else
-    url "https://github.com/msoap/shell2telegram/releases/download/1.0/shell2telegram-1.0.386.darwin.zip"
-    sha256 "43541ea5b8567fbdf4771725d848768d827aeb4c70efaf66e85d72a7479c9bfa"
+    url "https://github.com/msoap/shell2telegram/releases/download/1.1/shell2telegram-1.1.386.darwin.zip"
+    sha256 "313ef02473fef2358f2c86d7ee8c0356a26cb3b07c0843e895e936186d3b3ef5"
   end
 
   def install
