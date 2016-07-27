8fda421b9abd5a72545aee9c3d054e7e5120259b
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-07-27 20:10:19 +0300

    New version of go-carpet

--- go-carpet.rb
+++ go-carpet.rb
@@ -1,14 +1,14 @@
 class GoCarpet < Formula
   homepage "https://github.com/msoap/go-carpet"
   desc "Tool for viewing test coverage for Go source files\n\nTo view the test coverage in the terminal, just run go-carpet.\n\n"
-  version '1.3'
+  version '1.4'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/go-carpet/releases/download/1.3/go-carpet-1.3.amd64.darwin.zip"
-    sha256 "422a2c5805c15d3bc0c10a61fac68b4f55b01f8d57bc9a4f305217fe0ef0858c"
+    url "https://github.com/msoap/go-carpet/releases/download/1.4/go-carpet-1.4.amd64.darwin.zip"
+    sha256 "aa00a9aba591f9f6c83c3f7dd5799bf1f864e9cbc873b80e23dcffaaa204db2d"
   else
-    url "https://github.com/msoap/go-carpet/releases/download/1.3/go-carpet-1.3.386.darwin.zip"
-    sha256 "f4b0499a899abc11d48bb3537d2d1de4618865a56d658b9188de760ca0b53f4c"
+    url "https://github.com/msoap/go-carpet/releases/download/1.4/go-carpet-1.4.386.darwin.zip"
+    sha256 "cebe2184da2b9fb406e1197a44f58517957e4efe753a2f30d98cc61f4305ee22"
   end
 
   def install
