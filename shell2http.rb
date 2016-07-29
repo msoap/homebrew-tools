0febc4ef39e5595815f35dd409268317e6944f56
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-07-29 21:06:32 +0300

    New version of shell2http

--- shell2http.rb
+++ shell2http.rb
@@ -1,14 +1,14 @@
 class Shell2http < Formula
   homepage "https://github.com/msoap/shell2http"
   desc "Executing shell commands via simple http server\n\nSettings through 2 command line arguments, path and shell command.\n\n"
-  version '1.4'
+  version '1.5'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/shell2http/releases/download/1.4/shell2http-1.4.amd64.darwin.zip"
-    sha256 "e986aff9dd60da083d9f2803af37271f7e4e9f0ef5e0f22d2de768ea34058685"
+    url "https://github.com/msoap/shell2http/releases/download/1.5/shell2http-1.5.amd64.darwin.zip"
+    sha256 "ae0d1f335b40940a9fe657d22825d661a5b976f63d131a99b7b1afe2583ab9fd"
   else
-    url "https://github.com/msoap/shell2http/releases/download/1.4/shell2http-1.4.386.darwin.zip"
-    sha256 "2026bc8731843b620cc2f8005218e324e731736be05a069f1b088b954a3b62f7"
+    url "https://github.com/msoap/shell2http/releases/download/1.5/shell2http-1.5.386.darwin.zip"
+    sha256 "10cd41365e872194f52f41d810c32e4d0ed685a1d6854029eef43022f4d7e80a"
   end
 
   def install
