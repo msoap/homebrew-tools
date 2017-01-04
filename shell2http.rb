ad442e1fe09164b07fea1741c01322d0b4f92a70
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-01-04 10:50:08 +0200

    Fixed filename for shell2http

--- shell2http.rb
+++ shell2http.rb
@@ -4,10 +4,10 @@ class Shell2http < Formula
   version '1.7'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/shell2http/releases/download/1.7/shell2http-1.7.amd64.darwin.zip"
+    url "https://github.com/msoap/shell2http/releases/download/1.7/shell2http-1.7.darwin.amd64.zip"
     sha256 "b2a28d5e63c301c9d4a2a9d86f77fe450fe801ddf1055fbbcbbda4820a29bf30"
   else
-    url "https://github.com/msoap/shell2http/releases/download/1.7/shell2http-1.7.386.darwin.zip"
+    url "https://github.com/msoap/shell2http/releases/download/1.7/shell2http-1.7.darwin.386.zip"
     sha256 "cd5e8335310130d83d5e121cd9cd09817acc2b3683982cb9de687d2c62132633"
   end
 
