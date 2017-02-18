5ddcb78464754f27b00d69f8fd6e32adf99ae113
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-02-18 16:23:30 +0300

    New version of yandex-weather-cli

--- yandex-weather-cli.rb
+++ yandex-weather-cli.rb
@@ -1,14 +1,14 @@
 class YandexWeatherCli < Formula
   homepage "https://github.com/msoap/yandex-weather-cli"
   desc "☀️ ⛅️ ❄️ Command line interface for Yandex weather service\n\n"
-  version '1.7'
+  version '1.8'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/yandex-weather-cli/releases/download/1.7/yandex-weather-cli-1.7.darwin.amd64.tar.gz"
-    sha256 "3097ee5bd5b18c8f651bc88c98bbd8b8a098044817bd0700cdaf6828ef03365d"
+    url "https://github.com/msoap/yandex-weather-cli/releases/download/1.8/yandex-weather-cli-1.8.darwin.amd64.tar.gz"
+    sha256 "fdca9a65216d8fe27b6bad85323998a9a981afb755402b215ea462d9ae43f5e2"
   else
-    url "https://github.com/msoap/yandex-weather-cli/releases/download/1.7/yandex-weather-cli-1.7.darwin.386.tar.gz"
-    sha256 "adbf414315fef99e5c181742738bee3d2dfdb27dd6c15fffbc079ad5a33f899e"
+    url "https://github.com/msoap/yandex-weather-cli/releases/download/1.8/yandex-weather-cli-1.8.darwin.386.tar.gz"
+    sha256 "ef5f733377fbaa131e3ff17b1dd8aeb451bffcc1e2634c6a7f7a67251b00b201"
   end
 
   def install
