576ff643a042b41ad8dcfcefcc876a8fd472e1e1
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-02-18 15:12:18 +0300

    New version of html2data

--- html2data.rb
+++ html2data.rb
@@ -1,17 +1,18 @@
 class Html2data < Formula
   homepage "https://github.com/msoap/html2data"
   desc "Extract data from HTML via CSS selectors\n\n"
-  version '1.1'
+  version '1.2'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/html2data/releases/download/1.1/html2data-1.1.amd64.darwin.zip"
-    sha256 "48c752ccfdf883f1e9b902faae202ce748b66d4a3cbc35a8228ab51eeafc38b1"
+    url "https://github.com/msoap/html2data/releases/download/1.2/html2data-1.2.darwin.amd64.tar.gz"
+    sha256 "c4d046dccbc35efbe8c9d64c412512e3028de3108206fee0f304ada5a19ff410"
   else
-    url "https://github.com/msoap/html2data/releases/download/1.1/html2data-1.1.386.darwin.zip"
-    sha256 "478e464f23779307cb42ebf4002699b18dfa14cbf41e8634d75c36ba5d58ae7e"
+    url "https://github.com/msoap/html2data/releases/download/1.2/html2data-1.2.darwin.386.tar.gz"
+    sha256 "031df47850a2d038c45d40582b6e3ebd11eab80ff1fb4d3b16d8a620b81f906b"
   end
 
   def install
     bin.install "html2data"
+    man1.install "html2data.1"
   end
 end
