4d0806976f91896ba6195a200134017175a6e095
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-05-05 15:53:57 +0300

    New version of shell2telegram: 1.5

--- shell2telegram.rb
+++ shell2telegram.rb
@@ -1,14 +1,14 @@
 class Shell2telegram < Formula
   homepage "https://github.com/msoap/shell2telegram"
   desc "Create Telegram bot from command-line\n\n"
-  version '1.4'
+  version '1.5'
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/msoap/shell2telegram/releases/download/1.4/shell2telegram-1.4.darwin.amd64.tar.gz"
-    sha256 "07f7b4b5c4e5582d4653764e39c1c14d696df144216b49499ef52cd942ee4392"
+    url "https://github.com/msoap/shell2telegram/releases/download/1.5/shell2telegram-1.5.darwin.amd64.tar.gz"
+    sha256 "820dd215f4a2c53e6ac2a41b42bf1a0e4a0f7de4ad9d55c23d6966f9ce6ca201"
   else
-    url "https://github.com/msoap/shell2telegram/releases/download/1.4/shell2telegram-1.4.darwin.386.tar.gz"
-    sha256 "aefb898e6a328755209b299a01156c49c22f0cbf351e5987c23839b6ce9ceee4"
+    url "https://github.com/msoap/shell2telegram/releases/download/1.5/shell2telegram-1.5.darwin.386.tar.gz"
+    sha256 "2cbb743fd3f02d6f1b87dc03414139f1fe0a21a41db594a82632f03b31d86c42"
   end
 
   def install
