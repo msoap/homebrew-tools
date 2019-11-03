cffcb0018279033c8b97037da6d8c07ee31f9a00
Sergey Mudrik <sergey.mudrik@gmail.com>
2019-11-03 13:37:08 +0200

    Released the new version of yandex-weather-cli: 1.14

--- yandex-weather-cli.rb
+++ yandex-weather-cli.rb
@@ -1,14 +1,14 @@
 class YandexWeatherCli < Formula
   homepage "https://github.com/msoap/yandex-weather-cli"
   desc "☀️ ⛅️ ❄️ Command line interface for Yandex weather service\n\n"
-  version '1.13'
+  version '1.14'
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/msoap/yandex-weather-cli/releases/download/1.13/yandex-weather-cli-1.13.darwin.amd64.tar.gz"
-    sha256 "3a46e9788424540a0f13aa039b0d53779fe24ca59a93b77d766e315609618d27"
+    url "https://github.com/msoap/yandex-weather-cli/releases/download/v1.14/yandex-weather-cli-v1.14.darwin.amd64.tar.gz"
+    sha256 "164fa62a8d27a3b119b28f203c156298f633330ce2adb43945835dcb5ab9770d"
   else
-    url "https://github.com/msoap/yandex-weather-cli/releases/download/1.13/yandex-weather-cli-1.13.darwin.386.tar.gz"
-    sha256 "d42d4ab2e6d2b1805829f5d5d383eb2d644ba35c0dff780031a3555ba24336eb"
+    url "https://github.com/msoap/yandex-weather-cli/releases/download/v1.14/yandex-weather-cli-v1.14.darwin.386.tar.gz"
+    sha256 "476aa0bd3ce5067c3c62f533580f8292de64c2875c491cc0f67f8435e3942692"
   end
 
   def install
