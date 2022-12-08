ac10f365131d91f1ad36f2aad1e7d2139ebd65f5
Serhii Mudryk <sergey.mudrik@gmail.com>
2022-12-09 00:42:58 +0200

    Released new version of go-carpet: 1.10.0

--- go-carpet.rb
+++ go-carpet.rb
@@ -1,31 +1,31 @@
 class GoCarpet < Formula
   homepage "https://github.com/msoap/go-carpet"
   desc "Tool for viewing test coverage for Go source files\n\nTo view the test coverage in the terminal, just run go-carpet.\n\n"
-  version '1.9.0'
+  version '1.10.0'
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/msoap/go-carpet/releases/download/v1.9.0/go-carpet_1.9.0_darwin_amd64.tar.gz"
-      sha256 "ba6f71852bc32f3d554b106d8f0765616b1ea34c38f0a52d5c86f10e553d4f8b"
+      url "https://github.com/msoap/go-carpet/releases/download/v1.10.0/go-carpet_1.10.0_darwin_amd64.tar.gz"
+      sha256 "61ba55d7306694f0deced6363ed275e2339188da16ee6a71bee5a670a6357df4"
     elsif Hardware::CPU.arm?
-      url "https://github.com/msoap/go-carpet/releases/download/v1.9.0/go-carpet_1.9.0_darwin_arm64.tar.gz"
-      sha256 "e8c772e521cf9d25313c963ae0f2dfb49752c6233cf0c7ad99247a94771ebf2a"
+      url "https://github.com/msoap/go-carpet/releases/download/v1.10.0/go-carpet_1.10.0_darwin_arm64.tar.gz"
+      sha256 "14ec4256db17df93f13844e13c76fd86825bf45d683c7376b71a3eaf0e5b3441"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/go-carpet/releases/download/v1.9.0/go-carpet_1.9.0_linux_amd64.tar.gz"
-      sha256 "ba132adeb5f6425eb04355cca016b8277e7cdada12a5b8aee47d9ab81f8ba0e7"
+      url "https://github.com/msoap/go-carpet/releases/download/v1.10.0/go-carpet_1.10.0_linux_amd64.tar.gz"
+      sha256 "319d7e116493ebda1f506fb63800f2f7bab2b5dcdc9e89caf6f7f95c1b7baa2a"
     elsif Hardware::CPU.intel? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/go-carpet/releases/download/v1.9.0/go-carpet_1.9.0_linux_386.tar.gz"
-      sha256 "48bc78aaf5b2d3e482659eb46dc8599412c7404446bfeec4d2e3503f191ed035"
+      url "https://github.com/msoap/go-carpet/releases/download/v1.10.0/go-carpet_1.10.0_linux_386.tar.gz"
+      sha256 "a16473eae76270f455bb7605924e21302de93a96da40a5e0c7cc8de946adaa67"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/go-carpet/releases/download/v1.9.0/go-carpet_1.9.0_linux_arm64.tar.gz"
-      sha256 "0e866b9e6cf8505ecbb885de66ce8e4af067974f76c148fcd29ca08e60f4d460"
+      url "https://github.com/msoap/go-carpet/releases/download/v1.10.0/go-carpet_1.10.0_linux_arm64.tar.gz"
+      sha256 "89659c84e6714d846bb512c74cca6755a1e2cf3747fd6618341c4a956c12875c"
     elsif Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/go-carpet/releases/download/v1.9.0/go-carpet_1.9.0_linux_armv6.tar.gz"
-      sha256 "0aacd84aa5dcc74c23cf0486fb1b71077a62277978745d08c9e6b24c7044b247"
+      url "https://github.com/msoap/go-carpet/releases/download/v1.10.0/go-carpet_1.10.0_linux_armv6.tar.gz"
+      sha256 "c8024e1a3ba31d7253fdd5588179d387295cee1093fc133078d98c0200db5d10"
     end
   end
 
