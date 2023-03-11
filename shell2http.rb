50a7dcaab143dddb48db2b11651a1020be1ef61d
Serhii Mudryk <sergey.mudrik@gmail.com>
2023-03-11 19:15:51 +0200

    Released new version of shell2http: 1.16.0

--- shell2http.rb
+++ shell2http.rb
@@ -1,31 +1,31 @@
 class Shell2http < Formula
   homepage "https://github.com/msoap/shell2http"
   desc "Executing shell commands via simple http server\n\nSettings through 2 command line arguments, path and shell command.\n\n"
-  version '1.15.0'
+  version '1.16.0'
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/msoap/shell2http/releases/download/v1.15.0/shell2http_1.15.0_darwin_amd64.tar.gz"
-      sha256 "b016a7fd7dd52f97aa68e123dcea287e9ef982cd5ce2320cc5713cc0c17606c1"
+      url "https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_darwin_amd64.tar.gz"
+      sha256 "b1c28e5ad3549b7c2d3c603d49698830d8ec26e5beda01519659512535ffaeda"
     elsif Hardware::CPU.arm?
-      url "https://github.com/msoap/shell2http/releases/download/v1.15.0/shell2http_1.15.0_darwin_arm64.tar.gz"
-      sha256 "9c3332bc52f968bc95651620be9a2d34ae881e42e0ca3fa2df1db5c03d6d2398"
+      url "https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_darwin_arm64.tar.gz"
+      sha256 "3b59bee39c823392e3b52450e0323f76eb4ae22c91707840faca8834cbf8e288"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/shell2http/releases/download/v1.15.0/shell2http_1.15.0_linux_amd64.tar.gz"
-      sha256 "e807ec671d8297d7ca72ce2666ddddc7798793bd4dc107ebf1c7c98ae80c3f17"
+      url "https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_linux_amd64.tar.gz"
+      sha256 "2eb6dd4a59b7cec0a6e21506b48c00bb57c78229c4a2212729999ec3967e2523"
     elsif Hardware::CPU.intel? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/shell2http/releases/download/v1.15.0/shell2http_1.15.0_linux_386.tar.gz"
-      sha256 "184425cc3073885e0f7c4b4a6d4a9a8c2ee6275985432c94815db1bccd96e915"
+      url "https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_linux_386.tar.gz"
+      sha256 "c30510a6010ccf1e43d95a89c57f391a15c7cb5ef09cf7cd782cbcf3c870c1ee"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/shell2http/releases/download/v1.15.0/shell2http_1.15.0_linux_arm64.tar.gz"
-      sha256 "22e7e323fe4b2171a0a797d3b277b1b325a29ecf1a9a7422643c932e7ebdbf19"
+      url "https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_linux_arm64.tar.gz"
+      sha256 "aeaad90093c724aac4c50097674553c30bf6d12b318ca8f28a95179bab20c4ff"
     elsif Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/msoap/shell2http/releases/download/v1.15.0/shell2http_1.15.0_linux_armv6.tar.gz"
-      sha256 "94a0ed81173e86c7f64fd1f1674d52a4cac1a4721d42f29e58926655c63fb598"
+      url "https://github.com/msoap/shell2http/releases/download/v1.16.0/shell2http_1.16.0_linux_armv6.tar.gz"
+      sha256 "e626560c3ac49b05194be81b3b94e22c9b5cf28f1259032c7c881bf013b3a1fc"
     end
   end
 
